9da4deb8be2321ee433dd1f7a031fdb524a6b7e7
unknown <mati199111@gmail.com>
2019-12-14 12:28:08 +0100

    14122019:

---
+++ Calculator_v1/OperationType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_v1
+{
+    #region operationType
+    /// <summary>
+    /// a type of operation the calculator can perform
+    /// </summary>
+    public enum OperationType
+    {
+        /// <summary>
+        /// add two velue together
+        /// </summary>
+        Add,
+        /// <summary>
+        /// takes one value from another
+        /// </summary>
+        Minus,
+        /// <summary>
+        /// divide one number
+        /// </summary>
+        Divide,
+        /// <summary>
+        /// muliplies two numbers
+        /// </summary>
+        Multiply
+    }
+    #endregion
+}
